eaffecd84c73ccb022efb2c7a8868d6e4882840a
nathantipton <ntipton21@gmail.com>
2023-12-24 08:16:31 -0500

    need to fix light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -33,20 +33,21 @@ const config = {
     darkTheme: "myChristmasThemeDark",
     themes: [
       {
-        myChristmasTheme: {
-          primary: "#ef4444", // Red for primary elements
-          secondary: "#10b981", // Emerald for secondary elements
-          accent: "#facc15", // Yellow for accent elements
-          neutral: "#3b3b3b", // Dark gray for neutral elements
-          "base-100": "#ffffff", // White for base background
-          "base-200": "#f4f4f5", // Light gray for slightly darker base elements
-          "base-300": "#e5e7eb", // Medium gray for tertiary base elements
-          "base-content": "#f8f8f8", // Very light gray for primary content
-          info: "#60a5fa", // Blue for informational elements
-          success: "#22c55e", // Green for success elements
-          warning: "#fbbf24", // Yellow for warning elements
-          error: "#ef4444", // Red for error elements
-        },
+        // myChristmasTheme: {
+        //   primary: "#ef4444", // Red for primary elements
+        //   secondary: "#10b981", // Emerald for secondary elements
+        //   accent: "#facc15", // Yellow for accent elements
+        //   neutral: "#3b3b3b", // Dark gray for neutral elements
+        //   "base-100": "#ffffff", // White for base background
+        //   "base-200": "#f4f4f5", // Light gray for slightly darker base elements
+        //   "base-300": "#e5e7eb", // Medium gray for tertiary base elements
+        //   "base-content": "#1e293b", // Very light gray for primary content
+        //   "content-200": "#525252", // Dark gray for secondary content
+        //   info: "#60a5fa", // Blue for informational elements
+        //   success: "#22c55e", // Green for success elements
+        //   warning: "#fbbf24", // Yellow for warning elements
+        //   error: "#ef4444", // Red for error elements
+        // },
         myChristmasThemeDark: {
           primary: "#dc2626", // Red 600 for primary elements, a bold red
           secondary: "#16a34a", // Green 600 for secondary elements, a rich green
